40d0b59b9fdedf374472ba3aaaf86b3167bcb793
1273545169 <33993637+1273545169@users.noreply.github.com>
2018-09-19 15:24:03 +0800

    Add files via upload

---
+++ yolo/yolo/yolo_net.py
@@ -0,0 +1,280 @@
+# 此文件中包括：网络的建立和loss函数
+
+import numpy as np
+import tensorflow as tf
+import yolo.config as cfg
+
+slim = tf.contrib.slim
+
+
+class YOLONet(object):
+    def __init__(self, is_training=True):
+        self.classes = cfg.CLASSES
+        self.num_class = len(self.classes)
+        self.image_size = cfg.IMAGE_SIZE
+        self.cell_size = cfg.CELL_SIZE
+        self.boxes_per_cell = cfg.BOXES_PER_CELL
+        self.output_size = (self.cell_size * self.cell_size) * \
+                           (self.num_class + self.boxes_per_cell * 5)
+        self.scale = 1.0 * self.image_size / self.cell_size
+        self.boundary1 = self.cell_size * self.cell_size * self.num_class
+        self.boundary2 = self.boundary1 + \
+                         self.cell_size * self.cell_size * self.boxes_per_cell
+
+        self.object_scale = cfg.OBJECT_SCALE
+        self.noobject_scale = cfg.NOOBJECT_SCALE
+        self.class_scale = cfg.CLASS_SCALE
+        self.coord_scale = cfg.COORD_SCALE
+
+        self.learning_rate = cfg.LEARNING_RATE
+        self.batch_size = cfg.BATCH_SIZE
+
+        self.alpha = cfg.ALPHA
+
+        # shape(7,7,2),偏差
+        self.offset = np.transpose(np.reshape(np.array(
+            [np.arange(self.cell_size)] * self.cell_size * self.boxes_per_cell),
+            (self.boxes_per_cell, self.cell_size, self.cell_size)), (1, 2, 0))
+
+        self.images = tf.placeholder(
+            tf.float32, [None, self.image_size, self.image_size, 3],
+            name='images')
+        self.logits = self.build_network(
+            self.images, num_outputs=self.output_size, alpha=self.alpha,
+            is_training=is_training)
+
+        if is_training:
+            self.labels = tf.placeholder(
+                tf.float32,
+                [None, self.cell_size, self.cell_size, 5 + self.num_class])
+            self.loss_layer(self.logits, self.labels)
+            self.total_loss = tf.losses.get_total_loss()
+            tf.summary.scalar('total_loss', self.total_loss)
+
+    def build_network(self,
+                      images,
+                      num_outputs,
+                      alpha,
+                      keep_prob=0.5,
+                      is_training=True,
+                      scope='yolo'):
+        with tf.variable_scope(scope):
+            with slim.arg_scope(
+                    [slim.conv2d, slim.fully_connected],
+                    activation_fn=leaky_relu(alpha),
+                    weights_regularizer=slim.l2_regularizer(0.0005),
+                    weights_initializer=tf.truncated_normal_initializer(0.0, 0.01)
+            ):
+                # images的上下左右填充三层
+                net = tf.pad(
+                    images, np.array([[0, 0], [3, 3], [3, 3], [0, 0]]),
+                    name='pad_1')
+                net = slim.conv2d(
+                    net, 64, 7, 2, padding='VALID', scope='conv_2')
+                net = slim.max_pool2d(net, 2, padding='SAME', scope='pool_3')
+                net = slim.conv2d(net, 192, 3, scope='conv_4')
+                net = slim.max_pool2d(net, 2, padding='SAME', scope='pool_5')
+                net = slim.conv2d(net, 128, 1, scope='conv_6')
+                net = slim.conv2d(net, 256, 3, scope='conv_7')
+                net = slim.conv2d(net, 256, 1, scope='conv_8')
+                net = slim.conv2d(net, 512, 3, scope='conv_9')
+                net = slim.max_pool2d(net, 2, padding='SAME', scope='pool_10')
+                net = slim.conv2d(net, 256, 1, scope='conv_11')
+                net = slim.conv2d(net, 512, 3, scope='conv_12')
+                net = slim.conv2d(net, 256, 1, scope='conv_13')
+                net = slim.conv2d(net, 512, 3, scope='conv_14')
+                net = slim.conv2d(net, 256, 1, scope='conv_15')
+                net = slim.conv2d(net, 512, 3, scope='conv_16')
+                net = slim.conv2d(net, 256, 1, scope='conv_17')
+                net = slim.conv2d(net, 512, 3, scope='conv_18')
+                net = slim.conv2d(net, 512, 1, scope='conv_19')
+                net = slim.conv2d(net, 1024, 3, scope='conv_20')
+                net = slim.max_pool2d(net, 2, padding='SAME', scope='pool_21')
+                net = slim.conv2d(net, 512, 1, scope='conv_22')
+                net = slim.conv2d(net, 1024, 3, scope='conv_23')
+                net = slim.conv2d(net, 512, 1, scope='conv_24')
+                net = slim.conv2d(net, 1024, 3, scope='conv_25')
+                net = slim.conv2d(net, 1024, 3, scope='conv_26')
+                net = tf.pad(
+                    net, np.array([[0, 0], [1, 1], [1, 1], [0, 0]]),
+                    name='pad_27')
+                net = slim.conv2d(
+                    net, 1024, 3, 2, padding='VALID', scope='conv_28')
+                net = slim.conv2d(net, 1024, 3, scope='conv_29')
+                net = slim.conv2d(net, 1024, 3, scope='conv_30')
+                net = tf.transpose(net, [0, 3, 1, 2], name='trans_31')
+                net = slim.flatten(net, scope='flat_32')
+                net = slim.fully_connected(net, 512, scope='fc_33')
+                net = slim.fully_connected(net, 4096, scope='fc_34')
+                net = slim.dropout(
+                    net, keep_prob=keep_prob, is_training=is_training,
+                    scope='dropout_35')
+                net = slim.fully_connected(
+                    net, num_outputs, activation_fn=None, scope='fc_36')
+        return net
+
+    # boxes四个值为 x,y,w,h.都是绝对值（相对整张图片）
+    def calc_iou(self, boxes1, boxes2, scope='iou'):
+        """calculate ious
+        Args:
+          boxes1: 5-D tensor [BATCH_SIZE, CELL_SIZE, CELL_SIZE, BOXES_PER_CELL, 4]  ====> (x_center, y_center, w, h)
+          boxes2: 5-D tensor [BATCH_SIZE, CELL_SIZE, CELL_SIZE, BOXES_PER_CELL, 4] ===> (x_center, y_center, w, h)
+        Return:
+          iou: 4-D tensor [BATCH_SIZE, CELL_SIZE, CELL_SIZE, BOXES_PER_CELL]
+        """
+        with tf.variable_scope(scope):
+            # transform (x_center, y_center, w, h) to (x1, y1, x2, y2)
+            # shape(7,7,2,4)
+            boxes1_t = tf.stack([boxes1[..., 0] - boxes1[..., 2] / 2.0,
+                                 boxes1[..., 1] - boxes1[..., 3] / 2.0,
+                                 boxes1[..., 0] + boxes1[..., 2] / 2.0,
+                                 boxes1[..., 1] + boxes1[..., 3] / 2.0],
+                                axis=-1)
+
+            boxes2_t = tf.stack([boxes2[..., 0] - boxes2[..., 2] / 2.0,
+                                 boxes2[..., 1] - boxes2[..., 3] / 2.0,
+                                 boxes2[..., 0] + boxes2[..., 2] / 2.0,
+                                 boxes2[..., 1] + boxes2[..., 3] / 2.0],
+                                axis=-1)
+
+            # calculate the left up point & right down point
+            # shape(7,7,2,2)
+            lu = tf.maximum(boxes1_t[..., :2], boxes2_t[..., :2])
+            rd = tf.minimum(boxes1_t[..., 2:], boxes2_t[..., 2:])
+
+            # intersection
+            intersection = tf.maximum(0.0, rd - lu)
+            inter_square = intersection[..., 0] * intersection[..., 1]
+
+            # calculate the boxs1 square and boxs2 square
+            square1 = boxes1[..., 2] * boxes1[..., 3]
+            square2 = boxes2[..., 2] * boxes2[..., 3]
+
+            union_square = tf.maximum(square1 + square2 - inter_square, 1e-10)
+
+        return tf.clip_by_value(inter_square / union_square, 0.0, 1.0)
+
+    # predicts为模型预测值,shape(45,7,7,30)，labels为真实值,shape(45,7,7,25)
+    def loss_layer(self, predicts, labels, scope='loss_layer'):
+        with tf.variable_scope(scope):
+            # 预测值
+            # class-20
+            predict_classes = tf.reshape(
+                predicts[:, :self.boundary1],
+                [self.batch_size, self.cell_size, self.cell_size, self.num_class])
+            # confidence-2
+            predict_confidence = tf.reshape(
+                predicts[:, self.boundary1:self.boundary2],
+                [self.batch_size, self.cell_size, self.cell_size, self.boxes_per_cell])
+            # bounding box-2*4
+            predict_boxes = tf.reshape(
+                predicts[:, self.boundary2:],
+                [self.batch_size, self.cell_size, self.cell_size, self.boxes_per_cell, 4])
+
+            # 实际值
+            # shape(45,7,7,1)
+            # response中的值为0或者1.存在目标为1，不存在目标为0.
+            # 存在目标指的是存在目标的中心点，并不是说存在目标的一部分。所以，目标的中心点所在的cell其对应的值才为1，其余的值均为0
+            response = tf.reshape(
+                labels[..., 0],
+                [self.batch_size, self.cell_size, self.cell_size, 1])
+            # shape(45,7,7,1,4)
+            boxes = tf.reshape(
+                labels[..., 1:5],
+                [self.batch_size, self.cell_size, self.cell_size, 1, 4])
+            # shape(45,7,7,2,4),boxes的四个值，取值范围为0~1
+            boxes = tf.tile(
+                boxes, [1, 1, 1, self.boxes_per_cell, 1]) / self.image_size
+            # shape(45,7,7,20)
+            classes = labels[..., 5:]
+
+            # self.offset shape(7,7,2)
+            # offset shape(1,7,7,2)
+            offset = tf.reshape(
+                tf.constant(self.offset, dtype=tf.float32),
+                [1, self.cell_size, self.cell_size, self.boxes_per_cell])
+            # shape(45,7,7,2)
+            x_offset = tf.tile(offset, [self.batch_size, 1, 1, 1])
+            # shape(45,7,7,2)
+            y_offset = tf.transpose(offset, (0, 2, 1, 3))
+
+            # convert the x, y to the coordinates relative to the top left point of the image
+            # the predictions of w, h are the square root
+            # shape(45,7,7,2,4)  ->(x,y,w,h)
+            predict_boxes_tran = tf.stack(
+                [(predict_boxes[..., 0] + x_offset) / self.cell_size,
+                 (predict_boxes[..., 1] + y_offset) / self.cell_size,
+                 tf.square(predict_boxes[..., 2]),
+                 tf.square(predict_boxes[..., 3])], axis=-1)
+
+            # 预测box与真实box的IOU,shape(45,7,7,2)
+            iou_predict_truth = self.calc_iou(predict_boxes_tran, boxes)
+
+            # calculate I tensor [BATCH_SIZE, CELL_SIZE, CELL_SIZE, BOXES_PER_CELL]
+            # shape(45,7,7,1), find the maximum iou_predict_truth in every cell
+            # 在训练时，如果该单元格内确实存在目标，那么只选择IOU最大的那个边界框来负责预测该目标，而其它边界框认为不存在目标
+            object_mask = tf.reduce_max(iou_predict_truth, 3, keep_dims=True)
+            # object prosibility (45,7,7,2)
+            object_probs = tf.cast(
+                (iou_predict_truth >= object_mask), tf.float32) * response
+
+            # calculate no_I tensor [CELL_SIZE, CELL_SIZE, BOXES_PER_CELL]
+            # noobject prosibility(45,7,7,2)
+            noobject_probs = tf.ones_like(
+                object_probs, dtype=tf.float32) - object_probs
+
+            # shape(45,7,7,2,4)，对boxes的四个值进行规整，xy为相对于网格左上角，wh为取根号后的值，范围0~1
+            boxes_tran = tf.stack(
+                [boxes[..., 0] * self.cell_size - x_offset,
+                 boxes[..., 1] * self.cell_size - y_offset,
+                 tf.sqrt(boxes[..., 2]),
+                 tf.sqrt(boxes[..., 3])], axis=-1)
+
+            # class_loss shape(45,7,7,20)
+            class_delta = response * (predict_classes - classes)
+            class_loss = tf.reduce_mean(
+                tf.reduce_sum(tf.square(class_delta), axis=[1, 2, 3]),
+                name='class_loss') * self.class_scale
+
+            # object_loss  confidence=iou*p(object)
+            # p(object)的值为1或0
+            object_delta = object_probs * (predict_confidence - iou_predict_truth)
+            object_loss = tf.reduce_mean(
+                tf.reduce_sum(tf.square(object_delta), axis=[1, 2, 3]),
+                name='object_loss') * self.object_scale
+
+            # noobject_loss  p(object)的值为0
+            noobject_delta = noobject_probs * predict_confidence
+            noobject_loss = tf.reduce_mean(
+                tf.reduce_sum(tf.square(noobject_delta), axis=[1, 2, 3]),
+                name='noobject_loss') * self.noobject_scale
+
+            # coord_loss
+            coord_mask = tf.expand_dims(object_probs, 4)
+            boxes_delta = coord_mask * (predict_boxes - boxes_tran)
+            coord_loss = tf.reduce_mean(
+                tf.reduce_sum(tf.square(boxes_delta), axis=[1, 2, 3, 4]),
+                name='coord_loss') * self.coord_scale
+
+            tf.losses.add_loss(class_loss)
+            tf.losses.add_loss(object_loss)
+            tf.losses.add_loss(noobject_loss)
+            tf.losses.add_loss(coord_loss)
+
+            tf.summary.scalar('class_loss', class_loss)
+            tf.summary.scalar('object_loss', object_loss)
+            tf.summary.scalar('noobject_loss', noobject_loss)
+            tf.summary.scalar('coord_loss', coord_loss)
+
+            tf.summary.histogram('boxes_delta_x', boxes_delta[..., 0])
+            tf.summary.histogram('boxes_delta_y', boxes_delta[..., 1])
+            tf.summary.histogram('boxes_delta_w', boxes_delta[..., 2])
+            tf.summary.histogram('boxes_delta_h', boxes_delta[..., 3])
+            tf.summary.histogram('iou', iou_predict_truth)
+
+
+def leaky_relu(alpha):
+    def op(inputs):
+        return tf.nn.leaky_relu(inputs, alpha=alpha, name='leaky_relu')
+
+    return op
